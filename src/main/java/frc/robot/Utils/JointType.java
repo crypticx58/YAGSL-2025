20689d8a0a03335edac8de2d466f2611d4a12138
CrypticXSystem <82967269+crypticx58@users.noreply.github.com>
2025-02-14 14:38:50 -0500

    Change JointType Ids

--- src/main/java/frc/robot/Utils/JointType.java
+++ src/main/java/frc/robot/Utils/JointType.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import edu.wpi.first.math.util.Units;
 
 public enum JointType {
-    Shoulder(1, 150, 1, new Bound(0,200)),
-    Telescopic(2, 5, 1, new Bound(Units.inchesToMeters(25), Units.inchesToMeters(46.5))),
-    Wrist(3, 50, 1, new Bound(0,270)),
-    Intake(4, 1, 1, new Bound(0,360));
+    Shoulder(5, 150, 1, new Bound(0,200)),
+    Telescopic(6, 5, 1, new Bound(Units.inchesToMeters(25), Units.inchesToMeters(46.5))),
+    Wrist(7, 50, 1, new Bound(0,270)),
+    Intake(8, 1, 1, new Bound(0,360));
     public final int ID;
     public final double gearRatio;
     public final double maxSpeed; // DegreesPerSec for rotational or MetersPerSec for linear

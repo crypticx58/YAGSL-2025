120974ba960cb72be9a06208eb106509190d2a6d
CrypticXSystem <82967269+crypticx58@users.noreply.github.com>
2025-02-22 12:04:51 -0500

    Add intake sensor states to keep track of which sensors are blocked

---
+++ src/main/java/frc/robot/Utils/IntakeSensorsStates.java
@@ -0,0 +1,9 @@
+package frc.robot.utils;
+
+public class IntakeSensorsStates {
+    public final boolean EntranceSensorBlocked, ExitSensorBlocked;
+    public IntakeSensorsStates(boolean EntranceSensorBlocked, boolean ExitSensorBlocked){
+        this.EntranceSensorBlocked = EntranceSensorBlocked;
+        this.ExitSensorBlocked = ExitSensorBlocked;
+    }
+}

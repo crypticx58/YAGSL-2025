996248333c996e93c44266dccb81abc8c265f489
CrypticXSystem <82967269+crypticx58@users.noreply.github.com>
2025-03-10 16:12:03 -0400

    Add button mappings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,22 +5,32 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Utils.ArmPreset;
+import frc.robot.Utils.JointType;
+import frc.robot.Utils.InputsManager.ForwardKinematicsInputsManager;
 import frc.robot.commands.ArmControllerCommand;
+import frc.robot.commands.AutoAlignAlgae;
+import frc.robot.commands.GoToArmPreset;
+import frc.robot.commands.GoToCoralStationGrooveBasedOnPoseEstimation;
 import frc.robot.commands.GoToReefBasedOnPoseEstimation;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.OutakeCommand;
+import frc.robot.commands.ZeroArm;
 import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
-import frc.robot.utils.ArmPreset;
-import frc.robot.utils.JointType;
-import frc.robot.utils.InputsManager.ForwardKinematicsInputsManager;
 import swervelib.SwerveInputStream;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
@@ -36,13 +46,14 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem swerveSubsystem = SwerveSubsystem.getInstance();
   private final ArmSubsystem armSubsystem = ArmSubsystem.getInstance();
+  private final IntakeSubsystem intakeSubsystem = IntakeSubsystem.getInstance();
   private final VisionSubsystem visionSubsystem = VisionSubsystem.getInstance();
 
   private SendableChooser<Command> autoChooser;
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController driverXbox = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
   private final CommandXboxController armXbox = new CommandXboxController(OperatorConstants.ARM_CONTROLLER_PORT);
-  private final ForwardKinematicsInputsManager forwardKinematicsInputsManager = new ForwardKinematicsInputsManager(()->armXbox.getLeftY(), ()->-armXbox.getRightY(), ()->driverXbox.getRightTriggerAxis()-driverXbox.getLeftTriggerAxis());
+  private final ForwardKinematicsInputsManager forwardKinematicsInputsManager = new ForwardKinematicsInputsManager(()->armXbox.getLeftY(), ()->-armXbox.getRightY(), ()->armXbox.getLeftX());
   private final ArmControllerCommand armControllerCommand = new ArmControllerCommand(forwardKinematicsInputsManager);
   
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerveSubsystem.getSwerveDrive(),
@@ -55,6 +66,20 @@ public class RobotContainer {
 
   public RobotContainer() {
     // Configure the trigger bindings
+
+    NamedCommands.registerCommand("ZeroArm", new ZeroArm());
+    NamedCommands.registerCommand("ArmPresetL1", new GoToArmPreset(ArmPreset.L1));
+    NamedCommands.registerCommand("ArmPresetL2", new GoToArmPreset(ArmPreset.L2));
+    NamedCommands.registerCommand("ArmPresetL3", new GoToArmPreset(ArmPreset.L3));
+    NamedCommands.registerCommand("ArmPresetL4", new GoToArmPreset(ArmPreset.L4));
+
+    NamedCommands.registerCommand("GoToClosestLeftReef", new GoToReefBasedOnPoseEstimation(true));
+    NamedCommands.registerCommand("GoToClosestRightReef", new GoToReefBasedOnPoseEstimation(false));
+    NamedCommands.registerCommand("GoToClosestCoralStationFeed", new GoToCoralStationGrooveBasedOnPoseEstimation());
+
+    NamedCommands.registerCommand("Intake", new IntakeCommand());
+    NamedCommands.registerCommand("Outake", new OutakeCommand());
+
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Routine", autoChooser);
     configureBindings();
@@ -62,11 +87,27 @@ public class RobotContainer {
 
   private void configureBindings() {
     Command driveFieldOrientedAnglularVelocity = swerveSubsystem.driveFieldOriented(driveAngularVelocity);
-    driverXbox.b().onTrue(Commands.runOnce(() -> swerveSubsystem.zeroGyro(), swerveSubsystem));
-    driverXbox.x().whileTrue(new GoToReefBasedOnPoseEstimation(true, ArmPreset.L3));
-    driverXbox.a().whileTrue(new GoToReefBasedOnPoseEstimation(false, ArmPreset.L3));
 
-    armXbox.a().whileTrue(Commands.run(() -> {armSubsystem.setArmConfigurationOptimally(ArmPreset.CoralStationFeed.armConfiguration);}, armSubsystem));
+    driverXbox.b().onTrue(Commands.runOnce(() -> swerveSubsystem.zeroFieldOrientedHeading(driveAngularVelocity), swerveSubsystem));
+    
+    driverXbox.rightBumper().whileTrue(new GoToReefBasedOnPoseEstimation(false));
+    driverXbox.leftBumper().whileTrue(new GoToReefBasedOnPoseEstimation(true));
+    driverXbox.a().whileTrue(new GoToCoralStationGrooveBasedOnPoseEstimation());
+    driverXbox.y().whileTrue(new AutoAlignAlgae());
+    
+    armXbox.b().whileTrue(new ZeroArm().repeatedly());
+    armXbox.povDown().whileTrue(new GoToArmPreset(ArmPreset.L1).repeatedly());
+    armXbox.povRight().whileTrue(new GoToArmPreset(ArmPreset.L2).repeatedly());
+    armXbox.povUp().whileTrue(new GoToArmPreset(ArmPreset.L3).repeatedly());
+    armXbox.povLeft().whileTrue(new GoToArmPreset(ArmPreset.L4).repeatedly());
+    armXbox.a().whileTrue(new GoToArmPreset(ArmPreset.CoralStationFeed).repeatedly());
+    //armXbox.x().whileTrue(Commands.run(()->armSubsystem.setJointPosition(JointType.Shoulder, 90), armSubsystem));
+    //armXbox.a().whileTrue(Commands.run(()->armSubsystem.setJointPosition(JointType.Shoulder, 45), armSubsystem));
+
+
+    armXbox.rightTrigger(.10).whileTrue(Commands.runEnd(()->intakeSubsystem.setIntakeVelocity(armXbox.getRightTriggerAxis()/7), ()->intakeSubsystem.setIntakeVelocity(0), armSubsystem));
+    armXbox.leftTrigger(.10).whileTrue(Commands.runEnd(()->intakeSubsystem.setIntakeVelocity(-armXbox.getLeftTriggerAxis()/7), ()->intakeSubsystem.setIntakeVelocity(0), armSubsystem));
+
     armXbox.y().onTrue(Commands.runOnce(()-> {
       System.out.println("---------------------");
       System.out.println("Shoulder: "+armSubsystem.getJointPosition(JointType.Shoulder));
@@ -75,7 +116,7 @@ public class RobotContainer {
     }));
 
     swerveSubsystem.setDefaultCommand(driveFieldOrientedAnglularVelocity);
-    armSubsystem.setDefaultCommand(armControllerCommand);
+    //armSubsystem.setDefaultCommand(armControllerCommand);
     // if (Robot.isSimulation()) {
     //   driverXbox.a().onTrue(
     //       Commands.runOnce(() -> swerveSubsystem.swerveDrive.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));

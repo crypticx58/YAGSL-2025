c88cf55c0e7f309d5ca5b2ee50ab0220c548c954
CrypticXSystem <82967269+crypticx58@users.noreply.github.com>
2025-03-10 16:07:23 -0400

    Change ArmPresets

--- src/main/java/frc/robot/Utils/ArmPreset.java
+++ src/main/java/frc/robot/Utils/ArmPreset.java
@@ -1,12 +1,14 @@
-package frc.robot.utils;
+package frc.robot.Utils;
 
 import edu.wpi.first.math.util.Units;
 
 public enum ArmPreset {
-    L2(new ArmConfiguration(91.20332336425781, Units.inchesToMeters(23.499999459334248), 64.28636169433594)),
-    L3(new ArmConfiguration(97.75121307373047, Units.inchesToMeters(23.499999459334248), 12.095724105834961 )),
-    L4(new ArmConfiguration(84.92161560058594 , Units.inchesToMeters(51.98642963499535), 23.143321990966797)),
-    CoralStationFeed(new ArmConfiguration(-2.4373626708984375 , Units.inchesToMeters(23.499999459334248), 130.00033569335938));
+    Zero(new ArmConfiguration(JointType.Shoulder.initialValue+5, Units.inchesToMeters(23.499999459334248+0.5), JointType.Wrist.initialValue-1)),
+    L1(new ArmConfiguration(82.26927947998047, Units.inchesToMeters(24.12201851371705), 73.37083435058594)),
+    L2(new ArmConfiguration(82.26927947998047, Units.inchesToMeters(24.12201851371705), 73.37083435058594)),
+    L3(new ArmConfiguration(93.98094177246094, Units.inchesToMeters(23.499999459334248), 13.02807331085205)),
+    L4(new ArmConfiguration(89.66891479492188 , Units.inchesToMeters(51.20078409750631), 27.428071975708008)),
+    CoralStationFeed(new ArmConfiguration(20.83612632751465-1, Units.inchesToMeters(26.163008269362564), 96.6852798461914));
     public final ArmConfiguration armConfiguration;
     private ArmPreset(ArmConfiguration armConfiguration){
         this.armConfiguration = armConfiguration;

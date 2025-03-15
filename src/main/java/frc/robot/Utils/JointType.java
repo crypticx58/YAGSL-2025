9e9f7f744a12870de238330ceaf0dd0364ce5ac9
CrypticXSystem <82967269+crypticx58@users.noreply.github.com>
2025-03-15 01:41:13 -0400

    Change Wrist starting position to algae intake

--- src/main/java/frc/robot/Utils/JointType.java
+++ src/main/java/frc/robot/Utils/JointType.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.util.Units;
 public enum JointType {
     Shoulder(5, 1/150, 100, new Bound(-5,145), 0.5),
     Telescopic(6, 1/5, Units.inchesToMeters(24), new Bound(Units.inchesToMeters(23), Units.inchesToMeters(52.5)), Units.inchesToMeters(23.5)),
-    Wrist(7, 1/5, 180, new Bound(-180,180), 42+90);
+    Wrist(7, 1/5, 90, new Bound(-180,180), 180-48);
     public final int ID;
     public final double gearRatio;
     public final double maxSpeed; // DegreesPerSec for rotational or MetersPerSec for linear
